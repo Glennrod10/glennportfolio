d224a0f523a5ed60433d0f95cfcfb10af03d00af
glenn <glennrod10@gmail.com>
2026-07-04 14:28:27 +0530

    feat: add Google Analytics GA4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -88,6 +89,7 @@ export default function RootLayout({
         {children}
         <SpeedInsights />
         <Analytics />
+        <GoogleAnalytics gaId="G-556RFF7H2J" />
       </body>
     </html>
   );
